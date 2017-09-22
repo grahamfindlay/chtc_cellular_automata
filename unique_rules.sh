293951aaaa4b1f6560ff954d6050946d09445131
Graham Findlay <graham.b.findlay@gmail.com>
2017-09-22 09:53:55 -0500

    Queuing works

--- unique_rules.sh
+++ unique_rules.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
+OUTPUT_FILE="unique_rules.csv"
 N_CELLS=5
 
 UNIQUE_RULES=(0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 18 19 22 23 24 25 26 27 28 29 30 32 33 34 35 36 37 38 40 41 42 43 44 45 46 50 51 54 56 57 58 60 62 72 73 74 76 77 78 90 94 104 105 106 108 110 122 126 128 130 132 134 136 138 140 142 146 150 152 154 156 160 162 164 168 170 172 178 184 200 204 232)
 
-echo "unique_rules = ("
+echo "rule, z" >> $OUTPUT_FILE
 for rule in ${UNIQUE_RULES[@]}; do
-  for ((n_on = 0; n_on <= $N_CELLS; n_on++)); do
-    echo "  $rule,$n_on"
+  for ((z = 0; z <= $N_CELLS; z++)); do
+    echo "$rule, $z" >> $OUTPUT_FILE
   done
 done
-echo ")"
 
 
